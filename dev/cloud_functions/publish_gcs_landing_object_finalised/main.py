f695bf533f2598aa2542a4e5ff88daed87619c56
Jon Nixon <jonathan.yu.94@gmail.com>
2022-08-11 07:58:48 +1000

    Added cloud function for publishing to gcs_landing_object_finalised topic

---
+++ dev/cloud_functions/publish_gcs_landing_object_finalised/main.py
@@ -0,0 +1,29 @@
+# %% Import libraries
+import json
+import functions_framework
+from google.cloud import pubsub_v1
+
+# %% main function
+# Register a CloudEvent function with the Functions Framework
+@functions_framework.cloud_event
+def trigger_handler(cloud_event):
+    # Your code here
+    # Access the CloudEvent data payload via cloud_event.data
+    
+    project_id = "sandbox-egl1hjn"
+    topic_id = "gcs_landing_object_finalised"
+
+    publisher = pubsub_v1.PublisherClient()
+
+    # The `topic_path` method creates a fully qualified identifier
+    # in the form `projects/{project_id}/topics/{topic_id}`
+    topic_path = publisher.topic_path(project_id, topic_id)
+
+    # Publish message cloud event data
+    pubsub_message = json.dumps(cloud_event.data, indent=2).encode('utf-8')
+
+    # When you publish a message, the client returns a future.
+    future = publisher.publish(topic_path, pubsub_message)
+    print(future.result())
+
+    return 'OK'
