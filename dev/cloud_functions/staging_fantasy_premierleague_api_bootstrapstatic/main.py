fbdba32a2fd9db20f1bfbf95c0c5abe4ee895e31
Jon Nixon <jonathan.yu.94@gmail.com>
2022-08-11 21:22:48 +1000

    Updated staging_fantasy_premierleague_api_bootstrap_static cloud function

--- dev/cloud_functions/staging_fantasy_premierleague_api_bootstrapstatic/main.py
+++ dev/cloud_functions/staging_fantasy_premierleague_api_bootstrapstatic/main.py
@@ -1,5 +1,5 @@
 # %% Import libraries
-import base64,json
+import base64,json,datetime,re
 import functions_framework
 from google.cloud import storage
 
@@ -36,25 +36,45 @@ def download_blob_as_bytes(bucket_name, blob_name):
 def event_handler(cloud_event):
     # Your code here
     # Access the CloudEvent data payload via cloud_event.data
-    # Get name of bucket/bucket
-    pubsub_message_data = json.loads(base64.b64decode(cloud_event.data["message"]["data"]).decode())
+
+    staging_bucket_name = "fantasy-premier-league-staging"
+
+    # Get name of bucket/blob
+    pubsub_message_data = json.loads(base64.b64decode(cloud_event.data["message"]["data"]).decode('utf-8'))
     bucket_name = pubsub_message_data["bucket"]
     blob_name = pubsub_message_data["name"]
-    
+
+    # Check if updated blob is in folder:fantasy_premierleague_api_bootstrapstatic
+    if re.search("fantasy_premierleague_api_bootstrapstatic", blob_name)==None:
+        return 'OK'
+
     # Load blob as dictionary
     blob_data = json.loads(download_blob_as_bytes(bucket_name,blob_name))
 
     # Extract metadata
     source_url = blob_data["source_url"]
     source_date = blob_data["source_date"]
+    source_date_str = datetime.datetime.strftime(datetime.datetime.strptime(source_date, "%Y-%m-%d %H:%M:%S"),"%Y-%m-%d")
 
     # Export events data
+    events_blob_name = f"fantasy_premierleague_api_bootstrapstatic_events/source_date={source_date_str}/data00.jsonl"
     events = blob_data["data"]["events"]
+    events_str = "\n".join([json.dumps(event) for event in events])
+    events_bytes = events_str.encode('utf-8')
+    upload_blob(staging_bucket_name, events_bytes, events_blob_name)
 
     # Export teams
+    teams_blob_name = f"fantasy_premierleague_api_bootstrapstatic_teams/source_date={source_date_str}/data00.jsonl"
     teams = blob_data["data"]["teams"]
+    teams_str = "\n".join([json.dumps(team) for team in teams])
+    teams_bytes = teams_str.encode('utf-8')
+    upload_blob(staging_bucket_name, teams_bytes, teams_blob_name)
 
     # Export elements (players)
+    elements_blob_name = f"fantasy_premierleague_api_bootstrapstatic_elements/source_date={source_date_str}/data00.jsonl"
     elements = blob_data["data"]["elements"]
+    elements_str = "\n".join([json.dumps(element) for element in elements])
+    elements_bytes = elements_str.encode('utf-8')
+    upload_blob(staging_bucket_name, elements_bytes, elements_blob_name)
 
     return 'OK'
