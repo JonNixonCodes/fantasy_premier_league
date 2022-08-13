ef9b519201b0300147fc1d528269dec0c78c9c7b
Jon Nixon <jonathan.yu.94@gmail.com>
2022-08-13 20:06:15 +1000

    Added cloud functions for staging data

---
+++ dev/cloud_functions/staging_fantasy_premierleague_api_fixtures/main.py
@@ -0,0 +1,66 @@
+# %% Import libraries
+import base64,json,datetime,re
+import functions_framework
+from google.cloud import storage
+
+# %% Upload object
+def upload_blob(bucket_name, source_object, destination_blob_name):
+    """Uploads a file to the bucket."""
+    # The ID of your GCS bucket
+    # bucket_name = "your-bucket-name"
+    # The path to your file to upload
+    # source_file_name = "local/path/to/file"
+    # The ID of your GCS object
+    # destination_blob_name = "storage-object-name"
+
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
+
+    blob.upload_from_string(source_object)
+
+    print(
+        f"Data uploaded to {destination_blob_name}."
+    )
+
+def download_blob_as_bytes(bucket_name, blob_name):
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(blob_name)
+    return blob.download_as_bytes()
+      
+
+# %% main function
+# Register a CloudEvent function with the Functions Framework
+@functions_framework.cloud_event
+def event_handler(cloud_event):
+    # Your code here
+    # Access the CloudEvent data payload via cloud_event.data
+
+    staging_bucket_name = "fantasy-premier-league-staging"
+
+    # Get name of bucket/blob
+    pubsub_message_data = json.loads(base64.b64decode(cloud_event.data["message"]["data"]).decode('utf-8'))
+    bucket_name = pubsub_message_data["bucket"]
+    blob_name = pubsub_message_data["name"]
+
+    # Check if updated blob is in folder:fantasy_premierleague_api_fixtures
+    if re.search("fantasy_premierleague_api_fixtures", blob_name)==None:
+        return 'OK'
+
+    # Load blob as dictionary
+    blob_data = json.loads(download_blob_as_bytes(bucket_name,blob_name))
+
+    # Extract metadata
+    source_url = blob_data["source_url"]
+    source_date = blob_data["source_date"]
+    source_date_str = datetime.datetime.strftime(datetime.datetime.strptime(source_date, "%Y-%m-%d %H:%M:%S"),"%Y-%m-%d")
+
+    # Export fixtures
+    fixtures_blob_name = f"fantasy_premierleague_api_fixtures/source_date={source_date_str}/data00.jsonl"
+    fixtures = blob_data["data"]
+    fixtures_str = "\n".join([json.dumps(fixture) for fixture in fixtures])
+    fixtures_bytes = fixtures_str.encode('utf-8')
+    upload_blob(staging_bucket_name, fixtures_bytes, fixtures_blob_name)
+
+    return 'OK'
